dd104060b704d81721c8f816e204a0198913f404
n-suzuki-aaa <s47706560@gmail.com>
2024-06-02 09:42:51 +0900

    spec修正

--- rails_app/spec/models/user_spec.rb
+++ rails_app/spec/models/user_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  user = User.new(
+    name: "John",
+    email: "john@example.com",
+  )
+
   it "name と email を持ったユーザーを登録できること" do
-    user = User.new(
-      name: "John",
-      email: "john@example.com",
-      )
-     # オブジェクトをexpectに渡す動作が有効であることを確認
-     expect(user).to be_valid
+    # オブジェクトをexpectに渡す動作が有効であることを確認
+    expect(user).to be_valid
+  end
+
+  it "ユーザーの保存を行えること" do
+    # user.save が成功することを確認
+    expect { user.save }.to change { User.count }.by(1) 
   end
 end
